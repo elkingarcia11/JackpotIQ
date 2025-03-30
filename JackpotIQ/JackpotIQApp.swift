48eb11117713511ab83ae93e235dc7014aa2c5c6
Elkin Garcia <elkingarcia.11@gmail.com>
2025-03-29 22:04:00 -0400

    Initial Commit

---
+++ JackpotIQ/JackpotIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  JackpotIQApp.swift
+//  JackpotIQ
+//
+//  Created by Elkin Garcia on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct JackpotIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
